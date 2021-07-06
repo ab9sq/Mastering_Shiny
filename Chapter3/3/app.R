cbd9d4824c04250afb04664e77a890f0e38a2603
Lauerman <nick.lauerman@abbott.com>
2021-07-06 16:17:25 -0500

    chapter 3 sync

---
+++ Chapter3/3/app.R
@@ -0,0 +1,119 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+freqpoly <- function(x1, x2, binwidth=0.1, xlim=c(-3,3)){
+    df <- data.frame(
+        x = c(x1, x2),
+        g = c(rep("x1", length(x1)),
+              rep("x2", length(x2)))
+    )
+    ggplot(df, aes(x, color = g)) +
+        geom_freqpoly(binwidth = binwidth, size = 1)+
+        coord_cartesian(xlim = xlim)
+}
+
+t_test <- function(x1, x2){
+    test <- t.test(x1, x2)
+
+    #use springf() to format t.test() results compactly
+    sprintf(
+        "p value: %30f\n[%0.2f, %02f]",
+        test$p.value, test$conf.int[1], test$conf.int[2]
+    )
+}
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    fluidRow(
+        column(4,
+               "Distributiom 1",
+               numericInput("n1",
+                            label = "n",
+                            value = 1000,
+                            min = 1),
+               numericInput("mean1",
+                            label = "u",
+                            value = 0,
+                            step = 0.1),
+               numericInput("sd1",
+                            label = "o",
+                            value = 0.5,
+                            min = 0.1,
+                            step = 0.1)
+        ),
+        column(4,
+               "Distributiom 2",
+               numericInput("n2",
+                            label = "n",
+                            value = 1000,
+                            min = 1),
+               numericInput("mean2",
+                            label = "u",
+                            value = 0,
+                            step = 0.1),
+               numericInput("sd2",
+                            label = "o",
+                            value = 0.5,
+                            min = 0.1,
+                            step = 0.1)
+        ),
+        column(4,
+               "Frequency polygon",
+               numericInput("binwidth",
+                            label = "Bin width",
+                            value = 0.1,
+                            step = 0.1),
+               sliderInput("range",
+                           label = "range",
+                           value = c(-3, 3),
+                           min = -5,
+                           max = 5)
+        )
+
+    ),
+    fluidRow(
+        column(9,
+               plotOutput("hist")),
+        column(3,
+               verbatimTextOutput("ttest"))
+    )
+
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, server) {
+    output$hist <- renderPlot({
+        x1 <- rnorm(input$n1,
+                    input$mean1,
+                    input$sd1)
+        x2 <- rnorm(input$n2,
+                    input$mean2,
+                    input$sd2)
+
+        freqpoly(x1, x2,
+                 binwidth = input$binwidth,
+                 xlim = input$range)},
+        res = 96)
+
+      output$ttest <- renderText({
+         x1 <- rnorm(input$n1,
+                     input$mean1,
+                     input$sd1)
+         x2 <- rnorm(input$n2,
+                     input$mean2,
+                     input$sd2)
+         t_test(x1, x2)
+     })
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
